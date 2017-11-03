d5fdd7fc05c7c1834dfca75cc9a1484c04d7a0a5
Callum Gundlach <gundlachcallum@gmail.com>
2017-11-03 18:13:10 +0000

    Added dependency that genericIO needed

--- born/build_genericio.sh
+++ born/build_genericio.sh
@@ -2,6 +2,7 @@
 git clone http://zapad.Stanford.EDU/bob/genericIO.git ./genericIO/
 #module purge
 #module load gcc openmpi
+sudo yum -y install boost boost-devel
 mkdir genericIO/build
 cd genericIO/build
 cmake -Dhypercube_DIR=$1/hypercube/lib -DCMAKE_INSTALL_PREFIX=$1/genericIO ..
