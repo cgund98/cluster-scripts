3abfb4bd9f19da3a3596cbc31fb7cb88a3ac178d
Callum Gundlach <gundlachcallum@gmail.com>
2017-11-13 23:19:33 +0000

    Fixed move_data script to allow access to /mnt/resource

--- move_data.sh
+++ move_data.sh
@@ -5,7 +5,8 @@ for f in "$@"
 do
   if ! ${FIRST}
   then
-    scp -i ~/.ssh/cyclecloud/cyclecloud.pem $f cyclecloud@$1:/mnt/resource/
+    ssh -i ~/.ssh/cyclecloud/cyclecloud.pem cyclecloud@$1 "nohup sudo chown -R cyclecloud /mnt/resource; nohup sudo chmod -R u+rwx /mnt/resource"
+    scp -i ~/.ssh/cyclecloud/cyclecloud.pem $f cyclecloud@$1:/mnt/resource
   else
     FIRST=false
   fi
